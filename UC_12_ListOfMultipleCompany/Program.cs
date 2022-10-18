5dae6680ec42d057fd5f6e0b278ff86fbd9a6a2c
RamchandraPopatMulik <mulikram2016@gmail.com>
2022-10-18 03:00:03 -0400

    Added file

---
+++ UC_12_ListOfMultipleCompany/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace UC_12_ListOfMultipleCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWageBuilderArray empWageBuilder = new EmployeeWageBuilderArray();
+
+            Console.WriteLine(" Enter i to Enter Company Details or otherwise enter any key : ");
+            var input = Console.ReadLine();
+            while (input == "i")
+            {
+                var obj = UserInput();
+                empWageBuilder.addcompanyEmpWage(obj.company, obj.empRatePerHour, obj.numOfWorkingDays, obj.maxHoursPerMonth);
+                Console.WriteLine("Enter i to Enter Company Details or otherwise enter any key :");
+                input = Console.ReadLine();
+            }
+            empWageBuilder.computeEmpWage();
+        }
+        public static CompanyEmpWage UserInput()
+        {
+            Console.WriteLine("Enter Company Name : ");
+            string compName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Rate Per Hour : ");
+            int empRatePerHour = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter No of Working Days : ");
+            int numOfDays = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Maximum Hours Per Month : ");
+            int maxWorkingHourPerMonth = Convert.ToInt32(Console.ReadLine());
+
+            return new CompanyEmpWage(compName, empRatePerHour, numOfDays, maxWorkingHourPerMonth)
+            {
+                company = compName,
+                empRatePerHour = empRatePerHour,
+                numOfWorkingDays = numOfDays,
+                maxHoursPerMonth = maxWorkingHourPerMonth
+            };
+        }
+    }
+}
